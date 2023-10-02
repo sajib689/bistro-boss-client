095ef59deea18a8b782a7eee388b3485377e9a29
sajib689 <jibonraja11@gmail.com>
2023-10-02 11:18:57 +0600

    solve a error to af

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,9 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import { Helmet } from 'react-helmet-async';
+import AllUsersCard from '../AllUsersCard/AllUsersCard';
 
 const AllUsers = () => {
+   const {data: users = [], refetch} = useQuery(['users'], async () => {
+    const res = await fetch('http://localhost:3000/users')
+    return res.json()
+   })
     return (
-        <div>
-            
+        <div className=" w-full p-10">
+        <Helmet>
+          <title>Bistro Boss | All Users</title>
+        </Helmet>
+        <SectionTitle subHeading={'How many??'} heading={'MANAGE ALL USERS'}></SectionTitle>
+        <div className="uppercase font-semi-bold h-[40px] ">
+          <h3 className="text-3xl">Total Users: {users.length}</h3>
+          
         </div>
+        <div className="overflow-x-auto mt-7">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-[#D1A054] text-white ">
+              <tr className="uppercase">
+                <th></th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {users.map((user, index) => (
+                <AllUsersCard
+                  // handleDelete={handleDelete}
+                  key={user._id}
+                  index={index}
+                  user={user}
+                  refetch={refetch}
+                ></AllUsersCard>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
 };
 
---
+++ src/Pages/Dashboard/AllUsersCard/AllUsersCard.jsx
@@ -0,0 +1,80 @@
+import { FaRegTrashAlt, FaUserShield, FaUsers } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const AllUsersCard = ({ user, index, refetch }) => {
+  const {_id ,name, email } = user;
+
+  const handleDelete = _id => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.acknowledged === true) {
+              refetch();
+              Swal.fire(
+                "Deleted!",
+                "Your product has been deleted.",
+                "success"
+              );
+            }
+          });
+      }
+    });
+  };
+  const handleAdmin = _id => {
+    fetch(`http://localhost:3000/users/admin/${_id}`,{
+      method: 'PATCH',
+    
+    })
+    .then( res => res.json())
+    .then( data => {
+      console.log(data)
+      if(data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `${user.name} is an Admin Now`,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    })
+  }
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <th>{name}</th>
+      <td>{email}</td>
+      <td>
+        {user.role === "admin" ? (
+          "admin"
+        ) : (
+          <Link onClick={() => handleAdmin(_id)}>
+            {" "}
+            <FaUserShield className="text-[#D1A054] w-8 h-7 rounded-lg"></FaUserShield>
+          </Link>
+        )}
+      </td>
+      <td>
+        <Link onClick={() => handleDelete(user)}>
+          <FaRegTrashAlt className="text-red-600 w-8 h-7 rounded-lg"></FaRegTrashAlt>
+        </Link>
+      </td>
+    </tr>
+  );
+};
+
+export default AllUsersCard;
