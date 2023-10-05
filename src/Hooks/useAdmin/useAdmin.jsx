e6baf065d107db153a61b81c4447c951d773a0fb
sajib689 <jibonraja11@gmail.com>
2023-10-05 11:36:19 +0600

    admin problem solve

--- src/Hooks/useAdmin/useAdmin.jsx
+++ src/Hooks/useAdmin/useAdmin.jsx
@@ -4,20 +4,27 @@ import { useQuery } from "@tanstack/react-query"
 
 const useAdmin = () => {
     const token = localStorage.getItem('token')
-    const {user} = useContext(AuthContext)
-    const {data: isAdmin, isLoading: isAdminLoading} = useQuery({
+    const { user } = useContext(AuthContext)
+    const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
         queryKey: ['isAdmin', user?.email],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:3000/users/admin/${user?.email}`,{
+            const res = await fetch(`http://localhost:3000/users/admin/${user?.email}`, {
                 headers: {
                     authorization: `Bearer ${token}`
                 }
-            })
-            console.log(res.data)
-            return res.data.admin
+            });
+
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+
+            const responseData = await res.json();
+
+            return responseData.admin;
         }
-    })
-    return [isAdmin,isAdminLoading]
+    });
+    return [isAdmin, isAdminLoading];
 }
+
 
 export default useAdmin
